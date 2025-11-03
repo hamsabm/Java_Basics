466e654cb3f8f693c31ef791df63c796432898b9
HAMSA B M <hamsabm2005@gmail.com>
2025-11-03 15:20:39 +0530

    Create Arithmetic_Operations.java

---
+++ Arithmetic_Operations.java
@@ -0,0 +1,17 @@
+Perform all arithmetic operations on given two numbers num1,num2.
+
+import java.util.Scanner;
+public class Arith {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println("Sum: " + (a + b));
+        System.out.println("Difference: " + (a - b));
+        System.out.println("Product: " + (a * b));
+        System.out.println("Quotient: " + (a / b));
+        System.out.println("Remainder: " + (a % b));
+        sc.close();
+    }
+}
