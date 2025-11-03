aea485fe48291c9d71663b2aa59b65ffde8cadbd
HAMSA B M <hamsabm2005@gmail.com>
2025-11-04 00:26:30 +0530

    Create Division.java

---
+++ Division.java
@@ -0,0 +1,14 @@
+//Take two numbers num1 and num2 as input from stdin and you need to divide them.
+
+import java.util.Scanner;
+public class div
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        System.out.println(x/y);
+        sc.close();
+    }
+}
