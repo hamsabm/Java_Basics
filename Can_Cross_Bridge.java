fc87787d1df7138f00655550f848d1fdc0fe565e
HAMSA B M <hamsabm2005@gmail.com>
2025-11-04 00:10:52 +0530

    Update Can_Cross_Bridge.java

--- Can_Cross_Bridge.java
+++ Can_Cross_Bridge.java
@@ -1,8 +1,7 @@
 //You are given that a mango weighs X kilograms and a truck weighs Y kilograms. You want to cross a bridge that can withstand a weight of Z kilograms.
-Find the maximum number of mangoes you can load in the truck so that you can cross the bridge safely.
+//Find the maximum number of mangoes you can load in the truck so that you can cross the bridge safely.
 
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
