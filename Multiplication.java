89c2fdd2969b4fbb53712572410202258194b0c2
HAMSA B M <hamsabm2005@gmail.com>
2025-11-03 15:14:19 +0530

    Create Multiplication.java

---
+++ Multiplication.java
@@ -0,0 +1,15 @@
+Take two numbers num1 and num2 as input and you need to multiply them.
+
+import java.util.Scanner;
+public class Multiplication
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=x*y;
+        System.out.println(z);
+        sc.close();
+    }
+}
