fe533f3fcec405fef9f0e318d850414d84aa07e2
HAMSA B M <hamsabm2005@gmail.com>
2025-11-04 00:30:46 +0530

    Create Input_Print.java

---
+++ Input_Print.java
@@ -0,0 +1,13 @@
+//You just need to take input from stdin and print it to the stdout.
+
+import java.util.Scanner;
+public class str
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        System.out.println(str);
+        sc.close();
+    }
+}
